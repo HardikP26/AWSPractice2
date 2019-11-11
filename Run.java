ee25218060a24c1cc936e8c0bc51338213c6b8e3
HardikP26 <57611316+HardikP26@users.noreply.github.com>
2019-11-11 10:40:56 +0530

    Finish README
    Java File Updated

---
+++ Run.java
@@ -0,0 +1,31 @@
+package com.practice.javabasics;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Run
+{
+
+
+	public static void main(String[] args)
+	{
+
+		String s="C10O";
+        String Check_opt="C";
+        System.out.println(s.replace(Check_opt,""));
+        System.out.println(s.substring(1,s.length()));
+
+        
+       
+	
+	
+	}
+        
+		
+
+}
